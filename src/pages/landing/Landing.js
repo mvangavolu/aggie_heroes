95f78488a43aeadf9165bc442ea26abd98247600
iMentock <virgilmartinez@gmail.com>
2022-11-06 17:26:30 -0600

    feature - added landing page

---
+++ src/pages/landing/Landing.js
@@ -0,0 +1,15 @@
+import Psubjects from "./P_Subjects"
+import Hero from "./Hero"
+import TryUs from "./TryUs"
+import Testimonials from "./Testimonials"
+
+export default function Landing() {
+  return (
+    <div>
+      <Hero />
+      <TryUs />
+      <Psubjects />
+      <Testimonials />
+    </div>
+  )
+}
