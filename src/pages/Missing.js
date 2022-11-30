cdca32b88d3be911f92c904e7d6e80b55ce7aebe
iMentock <virgilmartinez@gmail.com>
2022-11-30 10:39:42 -0600

    feature - adding catch all page

---
+++ src/pages/Missing.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Missing() {
+  return (
+    <div className="centered_text my-5">
+      <h1>
+        <i class="fa-solid fa-magnifying-glass mx-3"></i>Ooops! Somethings
+        missing...
+      </h1>
+    </div>
+  )
+}
+
+export default Missing
